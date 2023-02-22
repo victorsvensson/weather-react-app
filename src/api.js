b006fc2bee8939faf14d5a5c0ae907c5bb47737b
Victor Svensson <80740775+victorsvensson@users.noreply.github.com>
2023-02-22 07:07:58 +0100

    Added local repository

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "ce0566e6b2mshd8acfb716aca8ecp179f9cjsne47e9df96ea3",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "85f605f66fed83b4cd172bf4c5259bfe";
+
+//85f605f66fed83b4cd172bf4c5259bfe
